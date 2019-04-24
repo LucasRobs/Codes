1a19da8079c3f47942f6c0dcad3dfd310905b5f8
LucasRobs <lucaskwo2@hotmail.com>
2019-04-24 14:09:58 -0300

    0.1

--- atividade02/blackjack.c
+++ atividade02/blackjack.c
@@ -1,77 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> 
-int main(){
-    srand(time(NULL));
-    int total = 0;
-    int as = 0;
-    int parada = 0;
-    int size = 0;
+char nome_da_carta(int carta){
+    if(carta == 1)
+        return 'A';
+    else if(carta == 10)
+        return 'f'; // letra que representa o 10 
+    else if(carta == 11)
+        return 'J';
+    else if(carta == 12)
+        return 'Q';
+    else if(carta == 13)
+        return 'K';
+    else
+        return carta + '0'; 
+}
 
-
-
-    int carta = 0;
-    int hist[20] = {0};
-    for(int i = 0;i < 20;i++){
-        hist[i] = 0;
+int valor_as(int soma){
+    if(soma > 21){
+        return soma -10;
+    }else{
+        return soma;
     }
-    for(int i = 0;i != -1;i++){
-        printf("Nova carta foi : ");
-        carta = rand() % (13 - 1 + 1) + 1;
-        
-        if(carta == 1)
-            printf("A\n");
-        else if(carta == 11)
-            printf("J\n");
-        else if(carta == 12)
-            printf("Q\n");
-        else if(carta == 13)
-            printf("K\n"); 
-        else
-            printf("%d\n", carta);    
-        if(carta > 9){
-            carta = 10;
-        }
-        if(carta == 1){
-            carta = 11;
-            as++;
-        }
-        hist[i] = carta;
-        total += carta;
-        printf("Soma total : %d \n", total);
-        if(total > 21){
-            puts("VOCE È UM MERDA MERMÂO");
-            break;
-        }
-        if(i <1){
-            
+}
+
+void print(char vet[]){
+    for(int i =0;vet[i] != '0' && vet[i] != '\0';i++){
+        if(vet[i] == 'f'){
+            printf("10 ");
         }else{
-        printf("Digite 0 = [hit] ou 1 = [parar]");
-        scanf("%d", &parada);
+            printf("%c ", vet[i]);
         }
-        if(parada == 1){
-            size = -1;
-        }
-        for(;total > 21 && as > 0;){
-            total -= 10;
-            as--;
-        }   
     }
-    printf("Histórico de cartas = [");
-    for(int i = 0; i <= size; i++){
-        if(hist[i] == 1)
-            printf("A");
-        else if(hist[i] == 11)
-            printf("J");
-        else if(hist[i] == 12)
-            printf("Q");
-        else if(hist[i] == 13)
-            printf("K");
-        else
-            printf("%d", hist[i]);
-        if(i != size - 1)
-            printf(", ");
-    }
+}
+void verifica_10(char dez){
+    if(dez == 'f')
+        printf("10");
+    else   
+        printf("%c", dez);
+    
+}
+int main(){
+    char maoM[21], maoJ[21];
+    for(int i = 0;i < 21; i++)
+        maoM[i] = maoJ[i] = '0';
+    srand(time(NULL));
+    int mesa = (rand() % 13) + 1,cartaM = 0,cartaJ = 0;
+    int somaM = mesa, somaJ = 0; 
+    maoM[0] = nome_da_carta(somaM);
+    printf("# Mesa recebe ");
+    verifica_10(maoM[0]);
+    printf(" - Total %d [ ",maoM[0],somaM);
+    print(maoM);
     printf("]\n");
-    printf("total final %d\n", total);
+
+    return 0; 
 }
