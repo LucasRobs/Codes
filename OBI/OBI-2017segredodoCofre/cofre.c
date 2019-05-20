ba575c3ecbc91a8d7756d11b8d60d0c7e7dd1abe
LucasRobs <lucaskwo2@hotmail.com>
2019-05-20 13:30:00 -0300

    0.1

---
+++ OBI/OBI-2017segredodoCofre/cofre.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int N = 0, M = 0;
+    scanf("%d %d",&N ,&M);
+    int vet[N];
+    for(int i = 0;i < N;i++){
+        scanf("%d", vet[i]);
+    }
+    int atual = 0;
+    for(int i = 0;i < M; i++){
+        autual = vet[i];
+        if
+    }
+}
