da5ae4ba0e4408de4784399e24b799216b29362d
LucasRobs <lucaskwo2@hotmail.com>
2019-06-03 12:11:00 -0300

    0.2

---
+++ jogos/JogoONE/v2/primeiroPlayer.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "gconio.h"
+#include "objeto.h"
+#include "tela.h"
+#include "menu.h"
+
+int main(){
+    Objeto * player = objeto_create(4, 5, '@');
+    Objeto * pedra = objeto_create(9, 7, '#');
+    Objeto * poste = objeto_create(10, 8, 'T');
+
+    int controle =  menu();
+    
+    if(controle == 1){
+        system("clear"); 
+        char tela[NL][NC];
+        for(;;){
+            char acao = getch();
+            //scanf("%c", &acao);
+            if(acao == 'x'){
+                system("clear"); 
+                puts("ADEUS ;-;");
+                return 0;
+            }
+            system("clear");  
+            objeto_move(player, acao);
+
+            tela_clear(tela);
+            tela_draw_border(tela);
+
+            tela_draw_object(tela, player);
+            tela_draw_object(tela, pedra);
+            tela_draw_object(tela, poste);
+
+            tela_print(tela);
+        }
+    }
+    objeto_destroy(player);
+    objeto_destroy(pedra);
+    objeto_destroy(poste);
+}
---
+++ jogos/JogoONE/v2/tela.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+int NL = 16;
+int NC = 32;
+
+void tela_clear(char tela[NL][NC]){
+    for(int l = 0; l < NL; l++)
+        for(int c = 0; c < NC; c++)
+            tela[l][c] = ' ';
+}
+
+void tela_print(char tela[NL][NC]){
+    for(int l = 0; l < NL; l++){
+        for(int c = 0; c < NC; c++)
+            printf("%c", tela[l][c]);
+        printf("\n");
+    }
+}
+
+void tela_draw_border(char tela[NL][NC]){
+    for(int l = 0; l < NL; l++){
+        tela[l][0] = '.';
+        tela[l][NC - 1] = '.';
+    }
+    for(int c = 0; c < NC; c++){
+        tela[0][c] = '.';
+        tela[NL - 1][c] = '.';
+    }
+}
+
+void tela_draw_object(char tela[NL][NC], Objeto * obj){
+    tela[obj->y][obj->x] = obj->nome;
+}
