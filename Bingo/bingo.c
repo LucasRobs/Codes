233af718018d1b40223ccdfd67f622b4ba4fe6c9
LucasRobs <lucaskwo2@hotmail.com>
2019-06-22 17:50:40 -0300

    bingo

--- Bingo/bingo.c
+++ Bingo/bingo.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-typedef struct{
-    char nome;
-    int tabela[5][5];
-} Tabela;
-
-Tabela * Tabela_create(char nome){
-    Tabela * tab = (Tabela*) malloc(sizeof(Tabela));
-    tab->nome = nome;
-    int maior = 0, menor = 0, fase[5]={15,30,45,60,75};
-    for(int y = 0;y < 5;y++){
-        maior = fase[y];
-        menor = maior - 14;
-        for(int x = 0; x < 5; x++){
-            tab->tabela[x][y] = rand() % (maior - menor + 1) + menor;
-            for(int z = x - 1;z >= 0; z--){
-                if(tab->tabela[x][y] == tab->tabela[z][y]){
-                    tab->tabela[x][y] = rand() % (maior - menor + 1) + menor;
-                    if(tab->tabela[x][y] == tab->tabela[z][y])
-                        tab->tabela[x][y] = rand() % (maior - menor + 1) + menor;
-                }
-            }
-        }
-    }
-    return tab;
-}  
-
-void Tabela_print(Tabela * tab){
-    printf("[B][I][N][G][O]\n");
-    for(int x = 0;x < 5;x++){
-        for(int y = 0; y < 5; y++)
-            printf("|%02d", tab->tabela[x][y]);
-        puts("|");
-    }
-}
-
+#include <unistd.h>
+#include "lib.h"
 
 int main(){
     srand(time(NULL));
     Tabela * cartela = Tabela_create('a');
-    Tabela_print(cartela);
+    int novoNumero = 0, Nbolas = 0, urna[75];
+    for(int i = 0; i < 75; i++){
+        urna[i] = 0;
+    }
+    while(1){
+        system("cls");
+        
+        Tabela_print(cartela);
+        int ganhou = bingo(cartela);
+        if(ganhou == 0){
+            puts("FIMM");
+            return 0;
+        }
+        novoNumero = rand() %  75 + 1;
+        Nbolas += 1;
+        for(int i = Nbolas; i > 0; i--){
+            if(novoNumero == urna[i]){
+                novoNumero = rand() %  75 + 1;
+                i = Nbolas;
+            }
+        }
+        urna[Nbolas -1] = novoNumero;
+        temNumero(novoNumero, cartela);
+        
+        sleep(2);
+
+        for(int i = 0; i < Nbolas; i++){
+            printf("%d ", urna[i]);
+        }
+        puts("");
+    }
+    
 }
